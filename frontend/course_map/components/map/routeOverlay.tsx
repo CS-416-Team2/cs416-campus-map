e30b6b8d697ede95f7cd28fe981dd1f12f55248f
jackson-trader <jackson.trader.dev@gmail.com>
2026-05-03 17:25:31 -0500

    style: update secondary brand color to golden rgba

--- frontend/course_map/components/map/routeOverlay.tsx
+++ frontend/course_map/components/map/routeOverlay.tsx
@@ -32,7 +32,7 @@ export function RouteOverlay() {
         source="route-source"
         layout={{ "line-join": "round", "line-cap": "round" }}
         paint={{
-          "line-color": "#006a61",
+          "line-color": "rgba(177, 129, 12, 1)",
           "line-width": ["interpolate", ["linear"], ["zoom"], 12, 3, 18, 8],
           "line-dasharray": [2, 2],
         }}
